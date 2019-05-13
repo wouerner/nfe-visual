d7cf759a068141b1105ff7a080098c2e289d28e9
Paulo Henrique <paulotux.mendes@gmail.com>
2019-05-13 13:42:41 -0300

    [UPDATE] Retornando tela com dados anteriores.

--- src/nfe/views/Notas.vue
+++ src/nfe/views/Notas.vue
@@ -1,85 +1,60 @@
 <template>
-  <v-container>
-      <v-btn to="/nfe/cadastrar">Cadastrar Nota</v-btn>
-  <v-data-table
-      :pagination.sync="pagination"
-      :headers="headers"
-      :items="notasGetter"
-      class="elevation-1"
-  >
-      <template v-slot:items="props">
-          <td class="">{{ props.item.infNFe.emit.xNome }}</td>
-          <td class="">{{ props.item.infNFe.dest.CPF }}</td>
-          <td class="">{{ props.item.infNFe.dest.xNome }}</td>
-          <td class="">{{ props.item.infNFe.ide.nNF }}</td>
-          <td class="">
-              <v-tooltip bottom>
-                  <v-btn
-                      slot="activator"
-                      flat
-                      icon
-                      to="/nfe/nota"
-                  >
-                      <v-icon>visibility</v-icon>
-                  </v-btn>
-                  <span>Visualizar</span>
-              </v-tooltip>
-          </td>
-      </template>
-  </v-data-table>
-  </v-container>
+    <v-container>
+        <v-btn to="/nfe/cadastrar">Cadastrar Nota</v-btn>
+        <v-data-table
+                :headers="headers"
+                :items="notasGetter"
+                class="elevation-1"
+        >
+            <template v-slot:items="props">
+                <td class="">{{ props.item._id }}</td>
+                <td class="">{{ props.item.infNFe.dest.CNPJ }}</td>
+                <td class="">{{ props.item.infNFe.dest.xNome }}</td>
+                <td class=""><v-btn to="/nfe/nota">Visualizar</v-btn></td>
+            </template>
+        </v-data-table>
+    </v-container>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
+    import { mapActions, mapGetters } from 'vuex';
 
-export default {
-data(){
-    return {
-        pagination: {
-            rowsPerPage: 10
-        },
-        headers: [
-            {
-              text: 'Emitente',
-              value: 'id',
-            },
-            {
-              text: 'CNPJ',
-              value: 'data',
-            },
-            {
-              text: 'Destinatario',
-              value: 'destinatario',
-            },,
-            {
-              text: 'N. Nota',
-              value: 'infNFe.ide.nNF',
-            },
-            {
-              text: 'Ação',
-              value: '',
+    export default {
+        data(){
+            return {
+                headers: [
+                    {
+                        text: 'ID',
+                        value: 'id',
+                    },
+                    {
+                        text: 'CNPJ',
+                        value: 'data',
+                    },
+                    {
+                        text: 'Destinatario',
+                        value: 'destinatario',
+                    }
+                ],
+                data: [
+
+                ]
             }
-        ],
-        data: [
-
-        ]
+        },
+        created(){
+            this.notasAction()
+        },
+        computed: {
+            ...mapGetters({
+                notasGetter: 'nota/notasGetter',
+            })
+        },
+        methods: {
+            ...mapActions({
+                notasAction: 'nota/notasAction',
+            })
+        }
     }
-},
-created(){
-   this.notasAction()
-},
-computed: {
-  ...mapGetters({
-      notasGetter: 'nota/notasGetter',
-  })
-},
-methods: {
-    ...mapActions({
-        notasAction: 'nota/notasAction',
-    })
-}
-}
 </script>
 
 <style>
